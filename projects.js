d596411e17c84e8905005a0485bd64e43b6c4457
mian0606 <mih037@ucsd.edu>
2026-05-01 23:36:09 -0700

    adjusted pie

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from './global.js';
 //data in lib
-const projects = await fetchJSON('./lib/projects.json');
+const projects = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
 
 renderProjects(projects, container, 'h2');
